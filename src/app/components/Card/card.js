af0a6f03f56b3de01f896e5d0964737e8120eee0
angelesrveron <angeles.veron03@gmail.com>
2024-05-16 18:54:51 -0300

    mensaje

--- src/app/components/Card/card.js
+++ src/app/components/Card/card.js
@@ -1,20 +1,22 @@
+'use client'
+import {useState} from 'react'
 import Image from "next/image";
 import styles from "./card.module.css";
 
-
 const Card = ({ image, title }) => {
- 
+const [message, setMessage] = useState('hola'); //destructuring de arrays, importa la posicion de cada uno de los valores dentro del array, de ese usestate hago un destructuring con las funcoines message y setmessage
+
+  const handleButtonClick = () => {
+    console.log("clickeado");
+    setMessage('chau');
+  };
+
   return (
     <div className={styles.container}>
-      <Image
-        src={image}
-        alt='character'
-        width={150}
-        height={150}
-        priority
-      />
+      <Image src={image} alt='character' width={150} height={150} priority />
       <h3>{title}</h3>
-      <button>Info</button>
+      <p>{message}</p>
+      <button onClick={handleButtonClick}>Info</button>
     </div>
   );
 };
--- src/app/components/FetchGrid/FetchGrid.js
+++ src/app/components/FetchGrid/FetchGrid.js
@@ -1,23 +1,32 @@
 'use client';
+import styles from "./FetchGrid.module.css";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 const FetchGrid = () => {
-    useEffect(()=>{
-        const handleGetData = async () => {
-          try {
+  const [albums, setAlbums] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+    useEffect(()=>{//cuando se monta el componente el useeffect ejecuta la funcion
+      setLoading(true);  
+      const handleGetData = async () => {//funcion asincronica pq trabajamos con una api rest la cual trabaja con promesas (funciones de js que no paran el cod hasta que se resuelva esa promesa, llama a la api y espera el resultado de la misma para mostrarla)
+          try {//si sale bien se ejecuta el try
             const response = await axios.get('https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Twenty+One+Pilots&api_key=d659cc02b91f8569a8d3a3e95091db43&format=json')
-            const data = response.data.topalbums.album;//en data nuestra info, nuestro array
-            console.log(data);
-          }catch (error){
+            const data = response.data.topalbums.album;//en data nuestra info, nuestro array, response axios deponde un objeto que nos devuelve a
+            setAlbums(data);
+            setLoading(false);
+            console.log(data);//get obtener la info
+          }catch (error){//si falla va al catch donde nos avisa el error
             console.log('el error es', error)
           }
         }
-        handleGetData() 
+        handleGetData() //ejecutamos la api
       },[])
     return(
-        <div>
-            <p>Fetching data</p>
+        <div className={styles.container}>
+          {loading && <p>Fetching data</p>}
+          {!loading && 
+          albums.map((album, index) => <p key={index}>{album.name}</p>)}
         </div>
     )
 }
